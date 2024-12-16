4674cb4e9f7343f354f2830f26dddf2f735b7c9a
jse <taxsilver331@gmail.com>
2024-12-16 10:56:32 +0900

    ajax chating

--- src/main/java/com/metacoding/refsocket/chat/ChatController.java
+++ src/main/java/com/metacoding/refsocket/chat/ChatController.java
@@ -1,11 +1,15 @@
 package com.metacoding.refsocket.chat;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
@@ -18,15 +22,22 @@ public class ChatController {
         return "index";
     }
 
+    @GetMapping("/save-form")
+    public String saveForm() {
+        return "save-form";
+    }
+
+    @GetMapping("/api")
+    public ResponseEntity<?> api(){
+        List<Chat> models = chatService.findAll();
+        return ResponseEntity.ok(models);
+    }
+
     @PostMapping("/chat")
     public String chat(String msg) {
         chatService.save(msg);
         return "redirect:/";
     }
 
-    @GetMapping("/save-form")
-    public String saveForm() {
-        return "save-form";
-    }
 
 }
